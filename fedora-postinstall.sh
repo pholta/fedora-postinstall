111214cec31d39bdec0d420bf5bbf3e87f777085
Jiri Folta <jiri.folta@centrum.cz>
2017-10-08 11:42:07 +0200

    script update
    gnome shell update mean that you need to run the script as a normal user

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -4,20 +4,20 @@
 # run this script as root just after you freshly installed the system
 
 # first, updating the system
-dnf update
+sudo dnf update
 
 # RPM Fusion repository - universal based on the distro version.. 
-dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y;
+sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y;
 
 # Audio Video codecs + VLC
-dnf install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1 vlc -y
+sudo dnf install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1 vlc -y
 
 # Libre office + Czech
-dnf install libreoffice libreoffice-langpack-cs -y
+sudo dnf install libreoffice libreoffice-langpack-cs -y
 
 # Google Chrome stable
 # Enable Google YUM repository
-cat << EOF > /etc/yum.repos.d/google-chrome.repo
+sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
 name=google-chrome - \$basearch
 baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
@@ -26,17 +26,17 @@ gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 
-dnf install google-chrome-stable -y
+sudo dnf install google-chrome-stable -y
 
 # Graphical stuff - Gimp (photo editor), Hugin (panoramic tool), Darktable (raw files processor)
-dnf install gimp hugin darktable -y
+sudo dnf install gimp hugin darktable -y
 
 # Gnome Tweak tool to customize advanced GNOME 3 options
-dnf install gnome-tweak-tool -y
+sudo dnf install gnome-tweak-tool -y
 
 #Steam for gaming
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
-dnf install steam -y
+sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
+sudo dnf install steam -y
 
 #Gnome Shell Tweaks
 #adding all buttons - minimize,maximize,close
